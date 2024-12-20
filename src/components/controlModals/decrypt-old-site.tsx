326842984b0d54c501f7541c071475f55c62bf83
npmzero <surajpathak3430@gmail.com>
2024-12-20 23:15:48 +0545

    Some designing issues on form

--- src/components/controlModals/decrypt-old-site.tsx
+++ src/components/controlModals/decrypt-old-site.tsx
@@ -78,7 +78,7 @@ export function DecryptSite({ params, encryptedData }: DecryptProps) {
                   <p className="text-red-500 text-sm">{error}</p>
                 )}
               </div>
-          <CardFooter className="flex gap-2">
+          <CardFooter className="flex gap-2 mt-4">
             <Button type="submit" >Decrypt</Button>
             <Link href="/">
               <Button type="button" variant="outline">Cancel</Button>
