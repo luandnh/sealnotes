3df88dc4f49e0ee30e31b7dc32c8cb2af8ea282d
harshsbhat <harsh121102@gmail.com>
2025-04-09 19:28:15 +0530

    Update

--- next.config.js
+++ next.config.js
@@ -16,6 +16,18 @@ const config = {
             key: 'Content-Security-Policy',
             value: "frame-ancestors 'none';",
           },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=(), fullscreen=()',
+          },
         ],
       },
     ];
